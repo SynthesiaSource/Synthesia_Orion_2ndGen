0deb67a82a733df787e6a2ebf48ce1e7240d0997
Vlad Lavrovsky <vlad@synthesia.ca>
2013-10-03 11:59:03 -0700

    First commit for 2nd generation Nexus.

---
+++ Synthesia_Orion_2ndGen.ino
@@ -0,0 +1,97 @@
+#include <SPI.h>
+#include <avr/sleep.h>
+#include "PinChangeInt.h"
+#include "pins.h"
+#include "batteryStatus.h"
+#include "orion.h"
+
+boolean poweredOn = false;
+
+//
+void togglePower(void) 
+{
+  noInterrupts();
+  poweredOn = !poweredOn;
+  
+  if(poweredOn)
+  {
+    updateBatteryStatus(poweredOn);
+    updateOrion();
+  }
+  interrupts();
+} // togglePower()
+
+
+void setup() 
+{
+  setupPins();
+  
+  // Make the ADC use the nternal 2.56v reference.
+  analogReference(INTERNAL);
+  
+  // The unit should be off on powerup.
+  poweredOn = false; 
+  
+  // Attach button interrupts
+  interrupts();
+  PCintPort::attachInterrupt(PIN_BUTTON_MODE, &stepMode, FALLING);
+  PCintPort::attachInterrupt(PIN_BUTTON_SPEED, &stepSpeed, FALLING);
+  attachInterrupt(INT1, &stepBrightness, FALLING);
+  attachInterrupt(INT0, &togglePower, FALLING);
+// Does not work. ???
+//  attachInterrupt(PIN_BUTTON_LEVEL, &stepBrightness, FALLING);
+//  attachInterrupt(PIN_BUTTON_POWER, &togglePower, FALLING);
+  
+  setupBatteryStatusInterrupt();  
+  setupOrion();
+} // setup()
+
+
+void loop() {
+
+  updateBatteryStatus(poweredOn);
+
+  // Start up the device
+  if(poweredOn && isDisabled())
+    enable(true);
+  
+  // Shut down the device
+  if(!poweredOn && isEnabled())
+  {
+    disable();
+    // Ensure that the status LED is off
+    forceStatusLightOff();
+    // Define sleep type
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+   // Attach the awake interrupt to the power button
+   //attachInterrupt(INT0,sleepHandler, FALLING); 
+   sleep_enable();
+   sleep_mode();  //sleep now
+   sleep_disable(); //fully awake now 
+  }
+
+  // Update the LEDs if the device is enabled
+  if(poweredOn)
+  {
+    updateOrion();
+  }
+
+
+} // loop()
+
+
+//// On-awake functions.
+//void sleepHandler() {
+//  
+////  // Remove the now superfluous interrupt
+////  detachInterrupt(INT0); 
+////  
+////  // Reinstate the regular one
+////  attachInterrupt(INT0, &togglePower, FALLING);
+//  
+//  // Turn the LEDs back on
+//  togglePower();
+//}
+
+// End of file.
+
